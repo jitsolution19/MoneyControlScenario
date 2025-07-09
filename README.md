59d7bdf881a519aa592505338a2b24d25d779237
jitendra <jitsolution19@gmail.com>
2025-07-09 15:20:43 +0530

    added maven Project

--- README.md
+++
@@ -1,2 +0,0 @@
-# MoneyControlScenario
-Automation of getting the stocks information from the moneycontrol.com site
---
+++ src/test/java/littlestation/Tc01HomePage.java
@@ -0,0 +1,22 @@
+package littlestation;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Tc01HomePage {
+	@Test
+	public void launchApplciation() {
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://littlestation.in/");
+		String actualText = driver.findElement(By.xpath(".//p[@class='wp-block-site-tagline has-small-font-size']"))
+				.getText();
+		System.out.println(actualText);
+		Assert.assertEquals("Little Treasures, Big Smiles!", actualText);
+		driver.quit();
+
+	}
+}
